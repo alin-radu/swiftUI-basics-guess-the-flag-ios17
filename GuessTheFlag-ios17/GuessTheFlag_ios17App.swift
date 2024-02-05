4c2fa010d1cab5313eaa8f92df529086bf2a15a7
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 20:44:37 +0200

    Initial Commit

---
+++ GuessTheFlag-ios17/GuessTheFlag_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_ios17App.swift
+//  GuessTheFlag-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
